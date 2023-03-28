59282793a48f7df669033cea2e15e4e881d5c243
ashutosh2406 <ashutosh.palai@theelixrlabs.com>
2023-03-28 18:31:23 +0530

    initial commit

---
+++ src/main/java/com/example/mhlogfileanalyser/MhLogFileAnalyserApplication.java
@@ -0,0 +1,13 @@
+package com.example.mhlogfileanalyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhLogFileAnalyserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MhLogFileAnalyserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mhlogfileanalyser/MhLogFileAnalyserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mhlogfileanalyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhLogFileAnalyserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
