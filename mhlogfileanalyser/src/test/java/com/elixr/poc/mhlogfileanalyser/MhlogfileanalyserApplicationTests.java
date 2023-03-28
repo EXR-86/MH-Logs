1de10f9b3eeea327d4245ef2e75efcb165093993
Ashutosh Palai <ashutosh.palai@theelixrlabs.com>
2023-03-28 12:21:29 +0530

    initial push

---
+++ mhlogfileanalyser/src/test/java/com/elixr/poc/mhlogfileanalyser/MhlogfileanalyserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elixr.poc.mhlogfileanalyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhlogfileanalyserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
